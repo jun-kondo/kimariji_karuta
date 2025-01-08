5068f2aeae4b6a38c173c244ea74a8ae936e7944
jun-kondo <ranmaru.oo.q@gmail.com>
2025-01-08 18:29:06 +0900

    chore: ESLintとPrettierの設定を追加

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+      },
+    },
+    rules: {},
+  },
+];
